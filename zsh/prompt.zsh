be221d2c4affae8018aae3278d6e4c268a757c41
Jeffrey Schang <schangj09@gmail.com>
2024-04-04 09:20:24 +0200

    remove battery status from command prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -66,7 +66,8 @@ battery_status() {
   fi
 }
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+#export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n💁‍♂️ $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
